611889a4aa0375353bcbe1c8c80808930d95036b
riznanasar003 <richurizna10@gmail.com>
2024-06-13 21:41:40 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div>
       <Login/>
+      <SignUp/>
     </div>
   );
 }
---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+            <center>
+                <h1>HEY !</h1>
+                <h6>Create a new account</h6>
+                </center>
+                <br/><br/>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">USERNAME</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">EMAIL ID</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">PHONE NUMBER</label>
+                        <input type="text" className="form-control" />
+
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">GENDER</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">PASSWORD</label>
+                        <input type="password" name="" id="" className="form-control" />
+
+
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">CONFIRM PASSWORD</label>
+                        <input type="password" name="" id="" className="form-control" />
+
+
+
+                        </div>
+                        <center>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <button className="btn btn-success">SIGN UP</button>
+
+                        </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default SignUp
